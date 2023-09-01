a01205043b27f3833a3e20387507e395d89277c8
Juan-Manuel-Flores <143746037+Juan-Manuel-Flores@users.noreply.github.com>
2023-09-01 13:02:52 -0500

    Add files via upload

---
+++ FLORES-JUANMANUEL-PYTHON-CH-8/FLORES-JUANMANUEL-CH-8.py
@@ -0,0 +1,4 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Juan/OneDrive/Desktop/PYTHON/FLORES-JUANMANUEL-PYTHON-CH-8/FLORES-JUANMANUEL-CH-8-EX-1.py
